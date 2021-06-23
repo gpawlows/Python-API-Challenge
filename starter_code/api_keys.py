fa3e219e3ab4cdf2c580168a1ff9c7321f4d5659
Geoff Pawlowski <gpawlows@gmail.com>
2021-06-22 22:34:01 -0700

    git commit -m

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "74b0ae8e020058c4d05f4785695fef47"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "626e1a8fdf10ed323ea4e49737d634f6a3b0503a"
